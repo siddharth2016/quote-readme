611ea09badee3640cb747d325bfd92c8c971bf9a
Siddharth2016 <siddharthchandragzb@gmail.com>
2020-10-11 21:11:07 +0530

    create test for scrap_quotes

---
+++ tests/test_scrap_quotes.py
@@ -0,0 +1,50 @@
+#tests/test_scrap_quotes.py
+"""
+Test for scrap_quotes.py
+"""
+
+from scripts.scrap_quotes import get_bs4_obj, get_ol_tags, get_all_quotes
+import unittest
+from unittest.mock import patch, Mock
+from bs4 import BeautifulSoup
+from bs4.element import Tag
+
+class TestScrapQuotes(unittest.TestCase):
+
+    def setUp(self):
+
+        self.html = """
+            <html>
+            <head></head>
+            <body>
+                <ol>
+                <li>some text</li>
+                </ol>
+            </body>
+            </html>
+        """
+        self.dummyurl = 'someurl.com'
+
+    def test_get_bs4_obj(self):
+        with patch('scripts.scrap_quotes.Request') as mockreq:
+            with patch('scripts.scrap_quotes.urlopen') as mockurlopen:
+                mockurlopen.return_value.read.return_value = self.html
+                actual = get_bs4_obj(self.dummyurl)
+                expected = BeautifulSoup(self.html, 'html.parser')
+                self.assertEqual(expected, actual)
+
+    def test_get_ol_tags(self):
+        bs4Obj = BeautifulSoup(self.html, 'html.parser')
+        actual = get_ol_tags(bs4Obj)
+        expected = bs4Obj.find_all('ol')
+        self.assertEqual(expected, actual)
+
+    def test_get_all_quotes(self):
+        bs4Obj = BeautifulSoup(self.html, 'html.parser')
+        ollist = bs4Obj.find_all('ol')
+        actual = list(get_all_quotes(ollist))
+        self.assertEqual(['some text'], actual)
+
+
+if __name__ == "__main__":
+    unittest.main()
